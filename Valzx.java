f6b8e82dc515e0cfe5a6e80fc7f1264f8efceea8
valzxswiftrade <only1valzx@gmail.com>
2024-09-18 10:38:56 +0100

    Add files via upload

---
+++ Valzx.java
@@ -0,0 +1,10 @@
+public class Third {
+    // MAIN METHOD
+
+    public static void main (String [] args) {
+        System.out.println("chidiebere");
+        System.out.println("welcome to Java class...");
+    }
+}
+
+    
